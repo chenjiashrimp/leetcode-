05a1b44a4341feea4b8af364b8793c96e35380b2
chenjiashrimp <72304789+chenjiashrimp@users.noreply.github.com>
2022-01-16 14:45:14 +0800

    Update 对称二叉树.java

--- easy/对称二叉树.java
+++ easy/对称二叉树.java
@@ -12,3 +12,35 @@ class Solution {
         return p.val==q.val&&check(p.left,q.right)&&check(p.right,q.left);
     }
 }
+
+class Solution {//使用迭代
+    public boolean isSymmetric(TreeNode root) {
+        return check(root,root);
+    }
+    public boolean check(TreeNode u,TreeNode v){
+        Queue<TreeNode> p=new LinkedList<TreeNode>();
+        p.offer(u);
+        p.offer(v);
+        while(!p.isEmpty()){
+            TreeNode u1=p.poll();
+            TreeNode v1=p.poll();
+            if(u1==null&&v1==null){
+                continue;//不能return true 因为未必遍历完
+            }
+            if(u1==null||v1==null||u1.val!=v1.val){
+                return false;
+            }
+            p.offer(u1.left);
+            p.offer(v1.right);
+            p.offer(u1.right);
+            p.offer(v1.left);
+        }
+        return true;
+    }
+}
+
+//补充 java队列
+//Queue
+//添加元素add() offer()超出容量会返回false，而add会异常
+//删除 remove() poll()同上
+//获取队头元素 element() peek()没有的话peek会返回null
