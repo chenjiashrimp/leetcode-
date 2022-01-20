40031641345748b39fbe7d2eee08ae3092824328
chenjiashrimp <72304789+chenjiashrimp@users.noreply.github.com>
2022-01-20 16:53:27 +0800

    Create 子集.java

---
+++ middle/子集.java
@@ -0,0 +1,23 @@
+class Solution {//递归搞定
+    public List<List<Integer>> subsets(int[] nums) {
+        return findsets(nums,nums.length-1);
+    }
+    public List<List<Integer>> findsets(int[] nums,int index){
+        if(index==0){//这里不知道怎么简化，能不能不设那么多变量，直接一步到位
+            List<List<Integer>> set=new ArrayList<List<Integer>>();
+            set.add(new ArrayList<Integer>());
+            List<Integer> list=new ArrayList<Integer>();
+            list.add(nums[0]);
+            set.add(list);
+            return new ArrayList<List<Integer>>(set);
+}
+        List<List<Integer>> sets=findsets(nums,index-1);
+        int size=sets.size();
+        for(int i=0;i<size;i++){
+            List<Integer> list=new ArrayList<Integer>(sets.get(i));
+            list.add(nums[index]);
+            sets.add(list);
+        }
+        return sets;
+    }
+}
