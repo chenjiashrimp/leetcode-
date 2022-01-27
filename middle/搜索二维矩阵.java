c80012f1ca7a4a6e20d47c4967ba8ae6cc0c3e0f
chenjiashrimp <72304789+chenjiashrimp@users.noreply.github.com>
2022-01-27 23:16:17 +0800

    Create 搜索二维矩阵.java

---
+++ middle/搜索二维矩阵.java
@@ -0,0 +1,19 @@
+class Solution {//一次二分查找，当然解析还有两次的，不过代码有些复杂
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int x = matrix[mid / n][mid % n];
+            if (x < target) {
+                low = mid + 1;
+            } else if (x > target) {
+                high = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
