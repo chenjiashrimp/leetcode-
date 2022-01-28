aba70dfad9fd6c5a6d064a2770f78ea653396090
chenjiashrimp <72304789+chenjiashrimp@users.noreply.github.com>
2022-01-28 11:23:27 +0800

    Create pow.java

---
+++ middle/pow.java
@@ -0,0 +1,15 @@
+class Solution {//递归
+    public double myPow(double x, int n) {
+        long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);//注意正负情况
+    }
+
+    public double quickMul(double x, long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;//分奇数偶数
+    }
+}
+
