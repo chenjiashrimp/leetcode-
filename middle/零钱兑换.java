762a87908f653f81b8a736c3244b65ae3b75b848
chenjiashrimp <72304789+chenjiashrimp@users.noreply.github.com>
2022-01-28 14:22:57 +0800

    Create 零钱兑换.java

---
+++ middle/零钱兑换.java
@@ -0,0 +1,15 @@
+class Solution {//动态规划，根本不用管重复不重复
+    public int coinChange(int[] coins, int amount) {
+        int[] f=new int[amount+1];
+        Arrays.fill(f,amount+1);
+        f[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.length;j++){
+                if(coins[j]<=i){
+                   f[i]=Math.min(f[i],f[i-coins[j]]+1);
+                }
+            }
+        }
+        return f[amount]>amount?-1:f[amount];
+    }
+}
