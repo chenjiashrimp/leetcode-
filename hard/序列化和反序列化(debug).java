c0201325f4f8eb8d5c220740778e7c954adefade
chenjiashrimp <72304789+chenjiashrimp@users.noreply.github.com>
2022-01-29 19:03:31 +0800

    Create 序列化和反序列化(debug).java

---
+++ hard/序列化和反序列化(debug).java
@@ -0,0 +1,36 @@
+public class Codec {//深度优先，把二叉树转为string再把string转为二叉树
+    public String serialize(TreeNode root) {
+        return rserialize(root, "");
+    }
+  
+    public TreeNode deserialize(String data) {
+        String[] dataArray = data.split(",");
+        List<String> dataList = new LinkedList<String>(Arrays.asList(dataArray));//把dataArray转化为list，但是只能读，不能增删
+        return rdeserialize(dataList);
+    }
+
+    public String rserialize(TreeNode root, String str) {
+        if (root == null) {
+            str += "None,";
+        } else {
+            str += str.valueOf(root.val) + ",";
+            str = rserialize(root.left, str);
+            str = rserialize(root.right, str);
+        }
+        return str;
+    }
+  
+    public TreeNode rdeserialize(List<String> dataList) {
+        if (dataList.get(0).equals("None")) {
+            dataList.remove(0);
+            return null;
+        }
+  
+        TreeNode root = new TreeNode(Integer.valueOf(dataList.get(0)));
+        dataList.remove(0);//要记得移掉
+        root.left = rdeserialize(dataList);
+        root.right = rdeserialize(dataList);
+    
+        return root;
+    }
+}
