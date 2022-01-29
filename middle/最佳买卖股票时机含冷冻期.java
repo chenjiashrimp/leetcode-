ce26a782fd4d9039097de8a3df83386e2e14f3e0
chenjiashrimp <72304789+chenjiashrimp@users.noreply.github.com>
2022-01-29 17:04:03 +0800

    Create 最佳买卖股票时机含冷冻期.java

---
+++ middle/最佳买卖股票时机含冷冻期.java
@@ -0,0 +1,16 @@
+class Solution {//难点在于如何划分子状态，注意下标的含义，第i天结束的状态
+    public int maxProfit(int[] prices) {
+        if(prices.length==0){
+            return 0;
+        }
+        int n=prices.length;
+        int[][] f=new int[n][3];
+        f[0][0]=-prices[0];
+        for(int i=1;i<n;i++){
+            f[i][0]=Math.max(f[i-1][0],f[i-1][2]-prices[i]);
+            f[i][1]=f[i-1][0]+prices[i];
+            f[i][2]=Math.max(f[i-1][1],f[i-1][2]);
+        }
+        return Math.max(f[n-1][1],f[n-1][2]);
+    }
+}
