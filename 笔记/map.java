8b5847ae8035b49e7f15b9707d458081dd0e0385
chenjiashrimp <72304789+chenjiashrimp@users.noreply.github.com>
2022-02-05 19:04:40 +0800

    Create map.java

---
+++ 笔记/map.java
@@ -0,0 +1,9 @@
+//哈希表 Map<,> =new HashMap<,>();  
+//构造函数 Hashtable() Hashtable(int size) Hashtable(Map m)
+//put(,) boolean contains(Object value) boolean containsKey(Object key) Object get(Object key) boolean isEmpty( ) Object remove(Object key) int size()
+
+//Map和Map.entry
+//Map.keySet()
+//Map.Entry<>   entrySet()
+//Map.values()
+//entry.getValue entry.getKey()
