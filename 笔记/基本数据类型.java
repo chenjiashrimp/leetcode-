e953f5ce749dc88d29dbf232f2b9d8f31c53e2dc
chenjiashrimp <72304789+chenjiashrimp@users.noreply.github.com>
2022-02-09 16:39:58 +0800

    Update 基本数据类型.java

--- 笔记/基本数据类型.java
+++ 笔记/基本数据类型.java
@@ -4,3 +4,11 @@
 //.charAt(index)
 //转化为char[] char[] =.toCharArray()
 //char[] 转为string  new string(char[])
+
+//string延申
+//StringBuilder和StringBuffer
+//StringBuffer 和 StringBuilder 类的对象能够被多次的修改，并且不产生新的未使用对象
+//由于 StringBuilder 相较于 StringBuffer 有速度优势，所以多数情况下建议使用 StringBuilder 类
+//method
+//.append("") .insert(开始的index,string) .delete(5,8)删除567 .deleteCharAt(index)
+//.toString()
